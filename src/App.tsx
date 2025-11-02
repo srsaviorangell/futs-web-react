49c47b833ea8ba84d2b506bc6fa25bfa6a99a628
savio rangel souza costa <sr188bet@gmail.com>
2025-11-02 05:36:43 -0300

    adicionei icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import './App.css'
+import logoFuts from "./assets/FuutLogo.png"
+import {Statistica, Homer, Perfil , AoVivo} from './icon/icones.tsx'
+import lampada from "./assets/Group.svg"
+
 
 function App() {
 
@@ -6,7 +10,16 @@ function App() {
     <>
     <div id="conteiner geral " className="bg-[#404040] w-screen h-screen flex justify-between items-center">
       
-      <header className=" w-18 h-full border "> menus e logo  </header>
+      <header className=" w-max h-max  "> 
+        <img src={logoFuts} alt="logo" className="w-15 h-15"/>  
+        <Homer/>
+        <AoVivo/>
+        <Statistica/>
+        <img src={lampada} alt="logo" className="w-15 h-15"/>  
+        <Perfil/>
+
+
+      </header>
       <main className="flex flex-col justify-between items-center w-full h-full">
         <nav className="border border-[#000000] w-full h-18">buscador e filtros</nav>
         <section className="border border-[#000000] w-full h-[98vh] ">
---
+++ src/icon/icones.tsx
@@ -0,0 +1,55 @@
+
+ export function Statistica(){
+    return(
+        <div className=" ">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-14">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z" />
+        </svg>
+        </div>
+    )
+}
+
+ export function Homer(){
+    return(
+        <div className="">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-14">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+        </svg>
+        </div>
+
+    )
+}
+
+export function AoVivo(){
+     return(
+        <div className="">
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className=" size-13 lucide lucide-radio-icon lucide-radio">
+            <path d="M16.247 7.761a6 6 0 0 1 0 8.478"/>
+            <path d="M19.075 4.933a10 10 0 0 1 0 14.134"/>
+            <path d="M4.925 19.067a10 10 0 0 1 0-14.134"/>
+            <path d="M7.753 16.239a6 6 0 0 1 0-8.478"/>
+            <circle cx="12" cy="12" r="2"/>
+        </svg>
+        </div>
+    
+    )  
+ }
+
+
+export function Perfil(){
+    return(    
+        <div className="">
+         
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-14">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+        </svg>
+        </div>
+ 
+     )  
+ }
+
+
+
+
+
